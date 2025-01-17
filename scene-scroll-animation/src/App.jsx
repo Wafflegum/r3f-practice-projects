0947228f4f1ac08e51102523380a93dc1c24c228
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2025-01-17 19:02:01 +0000

    Added shadows and started to customize the lights

--- scene-scroll-animation/src/App.jsx
+++ scene-scroll-animation/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import { ScrollControls, OrbitControls, Scroll } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Scene } from "../public/models/Scene";
+import { Scene } from "../public/models/Scene.jsx";
 
 function App() {
 	return (
 		<>
-			<Canvas>
+			<Canvas shadows >
+				<OrbitControls/>
 				<ScrollControls>
 					<Scroll>
-						<Scene scale={1} position={[0, 0, 0]} />
+						<Scene scale={1} position={[0, 0, 0]}/>
 					</Scroll>
 				</ScrollControls>
-				<directionalLight />
+				<pointLight position={[0, 0, -3.8]} castShadow intensity={5} color={"#ff7b00"}/>
+				<pointLight position={[0, 0.8, -3.8]} castShadow intensity={2} color={"#ffae000"}/>
+				<ambientLight intensity={0.1} color={"#6285ad"} />
 				{/* <ScrollControls></ScrollControls> */}
 			</Canvas>
 		</>
